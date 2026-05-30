0900a2f2d4f57dd4e7bacafebfbb69013bfb7edc
Antonioreyes8 <f2.arc.8@gmail.com>
2026-05-29 19:00:09 -0500

    Updating

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,17 +3,14 @@ import logo from "./assets/images/AlcalasRenovationsLogo.png";
 
 export default function Header() {
 	return (
-		<>
-			<header className="header">
-				<Link to="/">
-					<img src={logo} alt="logo" className="logo" />
-				</Link>
-			</header>
-
+		<header className="header">
+			<Link to="/">
+				<img src={logo} alt="logo" className="logo" />
+			</Link>
 			<nav className="nav">
 				<Link to="/">Home</Link>
 				<Link to="/contact">Contact</Link>
 			</nav>
-		</>
+		</header>
 	);
 }
